7ddf3b59bc37a9d883ac78f965e77ca8ce226827
angel <johnangelmx@gmail.com>
2022-06-05 03:13:36 -0500

    apt-get

--- install.sh
+++ install.sh
@@ -7,15 +7,22 @@ sudo apt-get autoremove -y
 sudo apt-get upgrade -y
 
 # Essentials
-sudo apt-get install -y \
-  bash zsh zgen sudo wget git make ca-certificates lsb-release \
-  vim nano libbrotli-dev cmake \
-  ccze jq less catimg zoxide \
-  tldr curl httpie man neofetch \
-  htop gtop ncdu icdiff \
-  unzip zip bzip2 p7zip-full \
-  locales locales-all \
-  bat exa lolcat
+sudo apt-get install -y bash
+sudo apt-get install -y zsh
+sudo apt-get install -y zgen
+sudo apt-get install -y sudo
+sudo apt-get install -y wget
+sudo apt-get install -y git
+sudo apt-get install -y make
+sudo apt-get install -y ca-certificates
+sudo apt-get install -y lsb-release
+sudo apt-get install -y vim nano libbrotli-dev cmake
+sudo apt-get install -y ccze jq less catimg zoxide
+sudo apt-get install -y tldr curl httpie man neofetch
+sudo apt-get install -y htop gtop ncdu icdiff
+sudo apt-get install -y unzip zip bzip2 p7zip-full
+sudo apt-get install -y locales locales-all
+sudo apt-get install -y bat exa lolcat
 # Fix batcat -> bat
 sudo ln -s /usr/bin/batcat /usr/local/bin/bat
 
