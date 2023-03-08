d870d7d67b137ab005f617aee7ac5dcbd3c8203b
Juan Angel <johnangelmx@gmail.com>
2023-03-08 11:37:06 -0600

    Modificacion-adaptacion-Ubuntu

--- README.md
+++ README.md
@@ -1,19 +1,8 @@
-# Debian 11 dotfiles
-Script post-instalacion o desplige , funcional con docker
+# Ubuntu 22.04.1 LTS
+Script post instalacion o desplige
 
 
-## Requisites
-
-If you haven't a non-root user with sudo privileges, create it before run this script:
-
-```bash
-sudo adduser angel --quiet
-sudo usermod -aG sudo angel
-sudo apt-get update -y && sudo apt-get install -y sudo git
-sudo su angel
-```
-
-## Installation
+## Instalacion
 
 ```bash
 git clone https://github.com/johnangelmx/.dotfiles.git ~/.dotfiles
--- install.sh
+++ install.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
-
-# Script for prepare Debian
-# export DEBIAN_FRONTEND=noninteractive
-# Update linux
-sudo apt-get autoremove -y
-sudo apt-get upgrade -y
-
-# Essentials
-sudo apt-get install -y bash zsh zgen sudo wget git \
+sudo apt get update -y && sudo apt get upgrade -y
+sudo add-apt-repository ppa:git-core/ppa -y
+# Escenciales
+sudo apt-get install -y bash zsh zgen sudo wget git ca-certificates lsb-release \
     make ca-certificates lsb-release \
-    vim nano libbrotli-dev cmake \
-    ccze jq less catimg \
-    tldr curl httpie man neofetch \
-    htop ncdu icdiff \
-    unzip zip bzip2 p7zip-full \
-    locales locales-all \
+    vim nano \
+    ccze nnn \
+    less \
+    tldr curl man neofetch \
+    htop icdiff\
+    unzip zip \
     bat exa 
-
-#
-sudo apt-get autoremove -y
-sudo apt-get upgrade -y
+# Re-Update
 sudo apt-get update -y
+sudo apt-get upgrade -y
 # Fix batcat -> bat
 sudo ln -s /usr/bin/batcat /usr/local/bin/bat
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 echo source $HOME/.dotfiles/.zshrc >>~/.zshrc
-# Node/NPM/PNPM install
+# Node/NPM/ install
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source $HOME/.nvm/nvm.sh
 nvm install --lts
-npm install gtop -g
 # Change to ZSH
 sudo chsh $USER -s /usr/bin/zsh
 
